25ec4dec809c1ec72b28763f7f5f201720d0ee56
baerenmarke90 <49028921+baerenmarke90@users.noreply.github.com>
2026-09-17 15:35:27 +0200

    style(web): load shared story detail treatment

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -29,6 +29,7 @@ import './components/ProfilePage.css';
 import './components/RelatedPeoplePage.css';
 import './components/RelatedPeopleAccessibility.css';
 import './components/StoryProductPages.css';
+import './components/StoryDetailContentFirst.css';
 import './components/StoryMomentMetadata.css';
 import './components/SharedPlanningSanctuary.css';
 import './components/SharedPlanningMotion.css';
